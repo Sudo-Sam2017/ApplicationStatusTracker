a16913a87a203d61b3a9f2e15191772b799d79e9
Sudo-Sam2017 <31296306+Sudo-Sam2017@users.noreply.github.com>
2021-01-23 00:47:03 -0500

    Created Database and Established Connection Created Model Class for Applications Created Sample Slider

---
+++ ApplicationStatusTracker/Models/Application.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApplicationStatusTracker.Models
+{
+    public class Application
+    {
+        public int ID { get; set; }
+        public string ApplicationName { get; set; }
+        public string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set;
+        }
+    }
+}
